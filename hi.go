c7f9cc9a8b1b99bf4c465678c05e8f363b9d8c90
Bobby Manchala <bobbymanchala@Bobbys-MacBook-Pro.local>
2025-08-19 17:05:24 -0700

    First Go program - Print student details

---
+++ hi.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string = "Priya"
+	var age int = 22
+	var city string = "Boston"
+	var isStudent bool = true
+	fmt.Println("Name:", name)
+	fmt.Println("Age:", age)
+	fmt.Println("City:", city)
+	fmt.Println("Is Student:", isStudent)
+}
