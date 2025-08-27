3bc785caf94851b497ba8293538baa3bce3ca1a2
bmanchala1507 <manchalababi@gmail.com>
2025-08-26 21:19:06 -0700

    Added main.go and maths.go

---
+++ Practise3/main.go
@@ -0,0 +1,76 @@
+//package main
+
+//import "fmt"
+
+//func main(){
+//name ,age, City := "Babi" , 25, "California"
+//isstudent:= true
+
+//fmt.Println("name:", name)
+//fmt.Println("age:", age)
+//fmt.Println("City:", City)
+//fmt.Println("isstudent:", isstudent)
+//}
+
+//package main
+
+//import "fmt"
+
+//func add(a int, b int) int {
+//	return a + b
+//}
+//func multiply(a int, b int) int {
+//	return a * b
+//}
+
+//func main() {
+
+//	sum := add(5, 3)
+//	multiply := multiply(5, 3)
+
+//	fmt.Println("sum:", sum)
+//	fmt.Println("multiply:", multiply)
+//}
+
+//package main
+
+//import "fmt"
+
+//func main() {
+//	var name string
+//	fmt.Println("Enter your name:")
+//	fmt.Scanln(&name)
+
+//	fmt.Println("Hello bobby welcome to the priya world.", name)
+//}
+
+//package main
+
+//import "fmt"
+
+//func main(){
+
+//	var name string ="Bobby"
+//	var age int =25
+
+//	fmt.Println("name", name)
+//	fmt.Println("age",age)
+//}
+
+//package main
+
+//import "fmt"
+
+//func main() {
+
+//	var firstname string
+//	var lastname string
+
+//	fmt.Println("Enter you first name")
+//	fmt.Scanln(&firstname)
+//	fmt.Println("Enter your last name")
+//	fmt.Scanln(&lastname)
+//	fmt.Println(" Hey priya , welcome to my world")
+//}
+
+
